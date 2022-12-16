f0f4057272749dbc8ec6aa596ecec8b369a63445
Adrian Mincu <mal13adi03@gmail.com>
2022-12-16 10:29:59 +0200

    finished problem 3

--- First Year/First Semester/Algoritm Programming/Labs/Lab 2022.12.15/pb3.py
+++ First Year/First Semester/Algoritm Programming/Labs/Lab 2022.12.15/pb3.py
@@ -10,17 +10,34 @@ def citire():
 def divide(x_st,y_jos,x_dr,y_sus,l_copaci):
     for c in copaci:
         if c[0] > x_st and c[0] < x_dr and c[1] > y_jos and c[1] < y_sus:
-           a1 = divide(x_st,c[1],x_dr,y_sus,l_copaci)
-           a2 = divide(x_st,y_jos,x_dr,c[1],l_copaci)
+            a1 = divide(x_st,c[1],x_dr,y_sus,l_copaci)
+            a2 = divide(x_st,y_jos,x_dr,c[1],l_copaci)
 
-           a3 = divide(x_st,y_jos,c[0],y_sus,l_copaci)
-           a4 = divide(c[0],y_jos,x_dr,y_sus,l_copaci)
-        break
+            a3 = divide(x_st,y_jos,c[0],y_sus,l_copaci)
+            a4 = divide(c[0],y_jos,x_dr,y_sus,l_copaci)
+
+            maxx = max(a1[4],a2[4],a3[4],a4[4])
+            if maxx == a1[4]:
+                return a1
+            if maxx == a2[4]:
+                return a2
+            if maxx == a3[4]:
+                return a3
+            if maxx == a4[4]:
+                return a4
+            break
     else:
         lungime = x_dr - x_st
         latime = y_sus - y_jos
         arie = lungime * latime
         return x_st,y_jos,x_dr,y_sus,arie
 
+def afisare(rez):
+    with open("pb3_dreptunghi.out",'w') as g:
+        g.write("Coordonate dreptunghi:\n")
+        g.write(f"({rez[0]}, {rez[1]})\n({rez[2]}, {rez[3]})\n\n")
+        g.write(f"Arie maxima: {rez[4]}")
+
 dreptunghi,copaci = citire()
-print(divide(dreptunghi[0][0],dreptunghi[0][1],dreptunghi[1][0],dreptunghi[1][1],copaci))
+sol = divide(dreptunghi[0][0],dreptunghi[0][1],dreptunghi[1][0],dreptunghi[1][1],copaci)
+afisare(sol)
