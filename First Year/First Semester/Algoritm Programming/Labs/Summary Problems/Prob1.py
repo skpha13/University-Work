ce3b07a4d90d5a05b770e4471947a8aebac5e149
Adrian Mincu <mal13adi03@gmail.com>
2022-12-27 15:26:12 +0200

    done problem 1

--- First Year/First Semester/Algoritm Programming/Labs/Summary Problems/Prob1.py
+++ First Year/First Semester/Algoritm Programming/Labs/Summary Problems/Prob1.py
@@ -9,3 +9,23 @@ def incarca_fisier(fisier):
     return l
 
 nume_fisier = input("nume fisier: ")
+ok = False
+l = incarca_fisier(nume_fisier)
+with open("egale.txt",'w') as g:
+    for line in range(len(l)):
+        x = min_max(l[line])
+        if x[0] == x[1]:
+            ok = True
+            g.write(str(line) + " ")
+    if ok == False:
+        g.write("Nu exista!")
+
+minn, maxx = min_max(l[0])
+for i in range(len(l)):
+    x = min_max(l[i])
+    if x[1] > maxx:
+        maxx = x[1]
+    if x[0] < minn:
+        minn = x[0]
+
+print(minn,maxx)
