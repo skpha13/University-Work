e829d048f43059d7f92ebc448f6ca3593b12ca17
Adrian Mincu <mal13adi03@gmail.com>
2022-12-27 13:14:35 +0200

    done subject 2

--- First Year/First Semester/Algoritm Programming/Labs/Test Simulation/Subiectul_2.py
+++ First Year/First Semester/Algoritm Programming/Labs/Test Simulation/Subiectul_2.py
@@ -15,7 +15,27 @@ def sterge_ore(lista,cinema,film,ora):
         if x[0] == cinema and x[1] == film:
             x[2].remove(ora)
 
+def cinema_film(lista,ora_minima,ora_maxima,*cinema,):
+    l_tupluri = []
+    lista.sort(key = lambda x: x[0])
+    cinema = list(cinema)
+    cinema.sort()
+    for i in cinema:
+        for j in lista:
+            if j[0] == i:
+                ore = []
+                for ora in j[2]:
+                    if ora >= ora_minima and ora <= ora_maxima:
+                        ore.append(ora)
+                if len(ore) != 0:
+                    l_tupluri.append((j[1],j[0],ore))
+    return l_tupluri
+
 date = citire_date()
-sterge_ore(date,input("cinema: "),input("film: "),input("ora: "))
-for line in date:
-    print(*line)
+#sterge_ore(date,input("cinema: "),input("film: "),input("ora: "))
+#for line in date:
+#    print(*line)
+l = cinema_film(date,"10:00","18:00","Cinema 1","Cinema 2")
+for line in l:
+    print(line)
+
