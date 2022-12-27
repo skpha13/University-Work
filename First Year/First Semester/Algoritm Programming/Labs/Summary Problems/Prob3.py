a03ba370f5213372358e465cebfefba60f11f3a7
Adrian Mincu <mal13adi03@gmail.com>
2022-12-27 17:59:30 +0200

    done point b

--- First Year/First Semester/Algoritm Programming/Labs/Summary Problems/Prob3.py
+++ First Year/First Semester/Algoritm Programming/Labs/Summary Problems/Prob3.py
@@ -7,4 +7,16 @@ def cifra_control(n):
         n = s
     return n
 
+def insereaza_cifra_control(l):
+    n = len(l)
+    i = 0
+    while i < n:
+        l.insert(i+1,cifra_control(l[i]))
+        n += 1
+        i += 2
+
+lista = [11,24,1,0]
+print(lista)
+insereaza_cifra_control(lista)
+print(lista)
 
