5d1dd2b6c426f38dd0c3623825e2fa1feb163c51
Adrian Mincu <mal13adi03@gmail.com>
2022-12-27 12:34:16 +0200

    fixed point b and done c

--- First Year/First Semester/Algoritm Programming/Labs/Test Simulation/Subiectul_1_b.py
+++ First Year/First Semester/Algoritm Programming/Labs/Test Simulation/Subiectul_1_b.py
@@ -1,19 +1,19 @@
 def prelucrare_lista(l):
     for i in range(len(l)):
-        minn = min(l[i])
-        for j in range(len(l[i])):
-            if l[i][j] == minn:
-                aux = j
-                break
-        l[i].pop(j)
+        aux = minn = 0
+        while aux == minn:
+            minn = min(l[i])
+            l[i].remove(minn)
+            aux = min(l[i])
+
     temp = []
     for line in l:
         temp.append(len(line))
     m = min(temp)
     for i in range(len(l)):
         if len(l[i]) > 1:
-            l[i] = l[i][:len(l[i])-m]                 
+            l[i] = l[i][:m]                
 
-l = [[1,2,3],[3,4],[54,-3,0,23]]
+l = [[100,54,101,54,2,81,92],[10,1,1,2,2,1,70],[12,81,10,8,9,8,10]]
 prelucrare_lista(l)
 print(l)
--- First Year/First Semester/Algoritm Programming/Labs/Test Simulation/Subiectul_1_c.py
+++ First Year/First Semester/Algoritm Programming/Labs/Test Simulation/Subiectul_1_c.py
@@ -6,6 +6,23 @@ def citire_numere():
             l.append(temp)
     return l
 
+def prelucrare_lista(l):
+    for i in range(len(l)):
+        aux = minn = 0
+        while aux == minn:
+            minn = min(l[i])
+            l[i].remove(minn)
+            aux = min(l[i])
+
+    temp = []
+    for line in l:
+        temp.append(len(line))
+    m = min(temp)
+    for i in range(len(l)):
+        if len(l[i]) > 1:
+            l[i] = l[i][:m]      
+
 M = citire_numere()
+prelucrare_lista(M)
 for line in M:
     print(*line)
