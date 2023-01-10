0f9d205e5f723f275db123fcc1247e8ac3e2c76f
Adrian Mincu <mal13adi03@gmail.com>
2023-01-10 17:09:34 +0200

    simpler solution

--- First Year/First Semester/Algoritm Programming/Labs/Lab 2022.10.20/3.py
+++ First Year/First Semester/Algoritm Programming/Labs/Lab 2022.10.20/3.py
@@ -2,7 +2,7 @@ s = input()
 i = 1
 
 for y in range(len(s)//2):
-    for x in range(i):
-        print(" ",end="")
-    print(s[i:-i])
+    # for x in range(i):
+    #     print(" ",end="")
+    print(s[i:-i].center(len(s)))
     i += 1
