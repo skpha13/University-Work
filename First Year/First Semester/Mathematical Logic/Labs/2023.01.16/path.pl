66b0cf8b0244aaf84467bd9538ff920b8a5a39c6
Adrian Mincu <mal13adi03@gmail.com>
2023-01-16 15:08:58 +0200

    Create path.pl

---
+++ First Year/First Semester/Mathematical Logic/Labs/2023.01.16/path.pl
@@ -0,0 +1,24 @@
+connected(1,2).
+connected(3,4).
+connected(5,6).
+connected(7,8).
+connected(9,10).
+connected(12,13).
+connected(13,14).
+connected(15,16).
+connected(17,18).
+connected(19,20).
+connected(4,1).
+connected(6,3).
+connected(4,7).
+connected(6,11).
+connected(14,9).
+connected(11,15).
+connected(16,12).
+connected(14,17).
+connected(16,19).
+
+path(X,X,[X]).
+path(X,Y,[X|L]) :- connected(X,Z), path(Z,Y,L).
+
+pathc(X,Y) :- path(X,Y,_).
