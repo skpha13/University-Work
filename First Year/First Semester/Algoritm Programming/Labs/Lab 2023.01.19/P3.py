fafc7669dad72c07821f1b1001096dc5d6523910
Adrian Mincu <mal13adi03@gmail.com>
2023-01-27 16:36:45 +0200

    finalized p3

--- First Year/First Semester/Algoritm Programming/Labs/Lab 2023.01.19/P3.py
+++ First Year/First Semester/Algoritm Programming/Labs/Lab 2023.01.19/P3.py
@@ -1,7 +1,20 @@
 with open("date.in") as f:
     l = [int(x) for x in f.readline().strip().split()]
 
+sume = []
+sume.append([l[0],l[0]])
 
+for i in range(1,len(l)):
+    if sume[i-1][0] + l[i] > l[i]:
+        sume.append([sume[i-1][0]+l[i]])
+        sume[i].extend(sume[i-1][1:])
+        sume[i].append(l[i])
+    else:
+        sume.append([l[i]])
+        sume[i].append(l[i])
+
+sol = [str(y) for y in max(sume,key=lambda x: x[0])[1:]]
+sol = " ".join(sol)
 
 with open("date.out",'w') as g:
-    pass
+    g.write(sol)
