97229018de4e90d92b16cdb23124799e729e4be3
Adrian Mincu <mal13adi03@gmail.com>
2023-01-28 16:00:54 +0200

    created permutari cu puncte fixe

---
+++ First Year/First Semester/Algoritm Programming/Labs/Backtracking/Perm_Pare_PF.py
@@ -0,0 +1,17 @@
+n = int(input("n: "))
+sol = [0] * (n + 1)
+use = [0] * (n + 1)
+
+def bkt(k):
+    for i in range(1,n + 1):
+        if use[i] == 0:
+            sol[k] = i
+            use[i] = 1
+            if sol[k]%2 == 0 and sol[k] == k:
+                if k == n:
+                    print(*sol[1:])
+                else:
+                    bkt(k+1)
+            use[i] = 0
+
+bkt(1)
