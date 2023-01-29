8e59f161159cc1d70648e2f59320f5e77bf136b6
Adrian Mincu <mal13adi03@gmail.com>
2023-01-29 10:32:16 +0200

    done point a

--- First Year/First Semester/Algoritm Programming/Exam Models/Exam Model 2023/S1.py
+++ First Year/First Semester/Algoritm Programming/Exam Models/Exam Model 2023/S1.py
@@ -1,2 +1,19 @@
 # a
 
+def divizori(*arg):
+    d = {}
+    for nr in arg:
+        aux = nr
+        f = 2
+        l = []
+        while aux > 1:
+            if aux % f == 0:
+                l.append(f)
+            while aux % f == 0:
+                aux //= f
+            f += 1
+        d[nr] = l
+    return d
+
+# b
+
