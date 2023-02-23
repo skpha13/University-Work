38ce2167e1a219cebec74bc2b3e0b3fb08e6eaf6
Adrian Mincu <mal13adi03@gmail.com>
2023-02-23 18:56:18 +0200

    tried something different

--- First Year/Second Semester/Data Structures/Lab1/Quick Sort/main.cpp
+++ First Year/Second Semester/Data Structures/Lab1/Quick Sort/main.cpp
@@ -6,26 +6,24 @@ using namespace std;
 ifstream f("algsort.in");
 ofstream g("algsort.out");
 
-int n,v[500001],temp[500001];
+int n,v[500001];
 
 void quicksort(int st,int dr)
 {
-    if(st<dr)
-    {
-        int m = (st+dr)/2;
-        int pivot = v[m];
-        int poz_left=st,poz_right=dr;
+    if(st>=dr) return;
 
-        for(int i=st;i<=dr;i++)
-            if(v[i]<pivot)
-                temp[poz_left++] = v[i];
-            else
-                temp[poz_right--] = v[i];
+    int pivot = v[dr],i=st-1;
 
-        for(int i=st;i<=dr;i++) v[i]=temp[i];
-        quicksort(st,poz_left-1);
-        quicksort(poz_right+1,dr);
-    }
+    for (int j = st; j < dr; j++)
+        if(v[j]<=pivot)
+        {
+            i++;
+            swap(v[i],v[j]);
+        }
+    swap(v[i+1],v[dr]);
+    i++;
+    quicksort(st,i-1);
+    quicksort(i+1,dr);
 }
 
 int main()
