7072275bee67bdf40e7ea4d15a3a7e181c43f06b
Adrian Mincu <mal13adi03@gmail.com>
2023-02-27 18:28:53 +0200

    :: operator

--- First Year/Second Semester/Object-oriented programming/Labs/2023.02.27/main.cpp
+++ First Year/Second Semester/Object-oriented programming/Labs/2023.02.27/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+class Calculator
+{
+private:
+    const int idCalculator;
+    char *nume_procesor;
+    string placa_video;
+    float pret;
+    int *p = new int[3];
+    int NrRam;
+    bool OnStock;
+    char ClasaEnergetica;
+
+public:
+    static int contorID; // o zona de memorie care e impartita pentru toate obiectele, daca modific ceva se modifica la toate obiectele
+};
+
+// operator de rezolutie(accesare)
+int Calculator::contorID=1000;
+
 int main()
 {
-    
+    cout<<Calculator::contorID;
     return 0;
 }
