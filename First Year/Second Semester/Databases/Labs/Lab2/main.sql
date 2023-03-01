783c53be4dd1c8360fa458b3955e3dd1967346c8
Adrian Mincu <mal13adi03@gmail.com>
2023-03-01 08:54:59 +0200

    finished first set of exercises

--- First Year/Second Semester/Databases/Labs/Lab2/main.sql
+++ First Year/Second Semester/Databases/Labs/Lab2/main.sql
@@ -19,5 +19,21 @@ from dual;
 select first_name || ' ' || last_name || ' castiga ' || salary || ' lunar dar doreste ' || salary*3 as "Salariu Ideal"
 from employees;
 
-select upper(substr(first_name,1,1)) || lower(substr(first_name,2,length(first_name))) 'Nume'
-from employees;
+select INITCAP(first_name) as Prenume,upper(last_name) as Nume,length(last_name) as "Lungime Nume"
+from employees
+where lower(substr(last_name,-1)) = 'j' or lower(substr(last_name,-1))='m' or lower(substr(last_name,3,1)) = 'a'
+order by length(last_name) desc;
+
+select INITCAP(first_name) as Prenume,upper(last_name) as Nume,length(last_name) as "Lungime Nume"
+from employees
+where lower(last_name) like 'j%' or lower(last_name) like 'm%' or lower(last_name) like '__a%' 
+order by length(last_name) desc;
+
+select employee_id,last_name,department_id
+from employees
+where lower(trim(both ' ' from first_name)) = 'steven';
+
+select employee_id,last_name,instr(lower(last_name),'a')
+from employees
+where lower(substr(last_name,-1)) = 'e';
+
