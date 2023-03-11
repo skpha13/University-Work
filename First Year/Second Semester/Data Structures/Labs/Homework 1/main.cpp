52eb898f49cb0ca7cc80c874afdfa0617184ae6d
Adrian Mincu <mal13adi03@gmail.com>
2023-03-11 12:05:31 +0200

    implemented swap sort

--- First Year/Second Semester/Data Structures/Labs/Homework 1/main.cpp
+++ First Year/Second Semester/Data Structures/Labs/Homework 1/main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <fstream>
+#include <time.h>
 
 using namespace std;
 
-ifstream f("numere.in");
-ofstream g("numere.out");
+ifstream f("teste.in");
+ofstream g("timp.out");
 
-int n,v[500001],temp[500001],i,j,mx=0;
+int *v = new int[1000000000];
+int *vcopy = new int[1000000000];
+int n,mx=0;
 
-void mergesort(int st,int dr)
+void generareFisierIn();
+
+void mergesort(int v[],int st,int dr)
 {
+    int *temp = new int[1000000000];
     if(st<dr)
     {
         int m  = (st + dr) / 2;
-        mergesort(st,m);
-        mergesort(m+1,dr);
+        mergesort(v,st,m);
+        mergesort(v,m+1,dr);
 
         int i=st,j=m+1,k=0;
         while(i<=m && j<=dr)
@@ -30,9 +36,15 @@ void mergesort(int st,int dr)
         i=st; j=0;
         while(i<=dr) v[i++]=temp[j++];
     }
+
+    if(temp != NULL)
+    {
+        delete[] temp;
+        temp = NULL;
+    }
 }
 
-void countsort()
+void countsort(int v[])
 {
     int *vf = new int[1000000000],k=0;
 
@@ -54,8 +66,16 @@ void countsort()
     }
 }
 
+void interschimbare(int v[])
+{
+    for(int i=0;i<n-1;i++)
+        for(int j=i;j<n;j++)
+            if(v[i]>v[j]) swap(v[i],v[j]);
+}
+
 int main()
 {
+    clock_t start,end;
     f>>n;
     for(int i=0;i<n;i++)
     {
@@ -63,9 +83,48 @@ int main()
         if(v[i]>mx) mx = v[i];
     }
 
-//    mergesort(0,n-1);
-    countsort();
-    for(int i=0;i<n;i++) g<<v[i]<<" ";
+    for(int i=0;i<n;i++) vcopy[i] = v[i];
+    start = clock();
+    mergesort(vcopy,0,n-1);
+    end = clock();
+    g<<"Mergesort: "<<(double)(end-start);
+    g<<endl;
+
+    for(int i=0;i<n;i++) vcopy[i] = v[i];
+    start = clock();
+    countsort(vcopy);
+    end = clock();
+    g<<"Countsort: "<<(double)(end-start);
+    g<<endl;
+
+    for(int i=0;i<n;i++) vcopy[i] = v[i];
+    start = clock();
+    interschimbare(vcopy);
+    end = clock();
+    g<<"Interschimbare: "<<(double)(end-start);
+    g<<endl;
+
+    if (v!=NULL)
+    {
+        delete[] v;
+        v = NULL;
+    }
+
+    if (vcopy!=NULL)
+    {
+        delete[] vcopy;
+        vcopy = NULL;
+    }
 
     return 0;
+}
+
+//putine numere
+//crescator
+//descrescator
+//5 cu destule numere
+//2 cu multe numere
+void generareFisierIn()
+{
+
 }
