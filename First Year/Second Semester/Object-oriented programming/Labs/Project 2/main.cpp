23301dd916f10fcd68d2c74fd91c670ec005c061
Adrian Mincu <mal13adi03@gmail.com>
2023-04-01 13:24:06 +0300

    first image opening

--- First Year/Second Semester/Object-oriented programming/Labs/Project 2/main.cpp
+++ First Year/Second Semester/Object-oriented programming/Labs/Project 2/main.cpp
@@ -1,6 +1,22 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace cv;
+int main()
+{
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if(img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    if(k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
     return 0;
 }
