f702e35c6bd61f55f474f80f29baea805d57a3fa
Adrian Mincu <mal13adi03@gmail.com>
2023-04-01 20:16:15 +0300

    started class Image

--- First Year/Second Semester/Object-oriented programming/Labs/Project 2/main.cpp
+++ First Year/Second Semester/Object-oriented programming/Labs/Project 2/main.cpp
@@ -2,9 +2,89 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <vector>
+
 using namespace cv;
+using std::string;
+using std::cout;
+using std::cin;
+using std::istream;
+using std::ostream;
+using std::endl;
+
+class Image {
+private:
+    string name,path,extension;
+    double rezolutieX,rezolutieY;
+public:
+    Image(string name = "Unnamed",string extension = "png",string path = "Root",double rezolutieX = 0,double rezolutieY = 0);
+    Image(const Image& obj);
+    Image& operator=(const Image& obj);
+    friend istream& operator>>(istream& in, Image& obj);
+    friend ostream& operator<<(ostream& out, const Image& obj);
+};
+
+Image::Image(string name, string extension, string path, double rezolutieX, double rezolutieY) {
+    this->name = name;
+    this->extension = extension;
+    this->path = path;
+    this->rezolutieX = rezolutieX;
+    this->rezolutieY = rezolutieY;
+}
+
+Image::Image(const Image &obj) {
+    this->name = obj.name;
+    this->extension = obj.extension;
+    this->path = obj.path;
+    this->rezolutieX = obj.rezolutieX;
+    this->rezolutieY = obj.rezolutieY;
+}
+
+Image& Image::operator=(const Image &obj) {
+    if (this != &obj)
+    {
+        if(!this->name.empty()) this->name.clear();
+        this->name = obj.name;
+        if(!this->extension.empty()) this->extension.clear();
+        this->extension = obj.extension;
+        if(!this->path.empty()) this->path.clear();
+        this->path = obj.path;
+        this->rezolutieX = obj.rezolutieX;
+        this->rezolutieY = obj.rezolutieY;
+    }
+    return *this;
+}
+
+istream& operator>>(istream& in, Image& obj) {
+    if(!obj.name.empty()) obj.name.clear();
+    if(!obj.path.empty()) obj.path.clear();
+    cout<<"Enter name: \n";
+    in>>obj.name;
+    cout<<"Specify file extension: \n";
+    in>>obj.extension;
+    cout<<"Enter path to image: \n";
+    in>>obj.path;
+    cout<<"Enter width: \n";
+    in>>obj.rezolutieX;
+    cout<<"Enter height: \n";
+    in>>obj.rezolutieY;
+
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Image& obj) {
+    out<<"Name: "<<obj.name<<"."<<obj.extension<<endl;
+    out<<"Path to image: "<<obj.path<<endl;
+    out<<"Width: "<<obj.rezolutieX<<" pixels\n";
+    out<<"Height: "<<obj.rezolutieY<<" pixels\n";
+
+    return out;
+}
+
 int main()
 {
+//    OPENCV TEST
+    /*
     std::string image_path = samples::findFile("starry_night.jpg");
     Mat img = imread(image_path, IMREAD_COLOR);
     if(img.empty())
@@ -18,5 +98,18 @@ int main()
     {
         imwrite("starry_night.png", img);
     }
+     */
+
+//    IMAGE TESTS
+    /*Image i;
+    cout<<i<<endl;
+    cin>>i;
+    cout<<i<<endl;
+    Image i2(i);
+    cout<<i2<<endl;
+    Image i3;
+    i3 = i2;
+    cout<<i3<<endl;*/
+    
     return 0;
 }
