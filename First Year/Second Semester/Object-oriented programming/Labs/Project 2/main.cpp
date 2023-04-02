d2480bc9a5316f00a5d11567a2124ed16cce54c6
Adrian Mincu <mal13adi03@gmail.com>
2023-04-02 20:45:18 +0300

    done class adjustments

--- First Year/Second Semester/Object-oriented programming/Labs/Project 2/main.cpp
+++ First Year/Second Semester/Object-oriented programming/Labs/Project 2/main.cpp
@@ -83,25 +83,22 @@ ostream& operator<<(ostream& out, const Image& obj) {
 
 class Effect:public Image {
 private:
-    string name;
     bool effect;
 public:
-    Effect(string nameImage = "Unnamed",string extension = "png",string path = "Root",double rezolutieX = 0,double rezolutieY = 0,string name = "",bool effect = 0);
+    Effect(string name = "Unnamed",string extension = "png",string path = "Root",double rezolutieX = 0,double rezolutieY = 0,bool effect = 0);
     Effect(const Effect& obj);
     Effect& operator=(const Effect& obj);
     friend istream& operator>>(istream& in, Effect& obj);
     friend ostream& operator<<(ostream& out, const Effect& obj);
 };
 
-Effect::Effect(string nameImage, string extension, string path, double rezolutieX, double rezolutieY, string name, bool effect):
-    Image(nameImage,extension,path,rezolutieX,rezolutieY)
+Effect::Effect(string name, string extension, string path, double rezolutieX, double rezolutieY, bool effect):
+    Image(name,extension,path,rezolutieX,rezolutieY)
 {
-    this->name = name;
     this->effect = effect;
 }
 
 Effect::Effect(const Effect &obj):Image(obj) {
-    this->name = obj.name;
     this->effect = obj.effect;
 }
 
@@ -109,7 +106,6 @@ Effect& Effect::operator=(const Effect &obj) {
     if(this != &obj)
     {
         Image::operator=(obj);
-        this->name = obj.name;
         this->effect = obj.effect;
     }
     return *this;
@@ -117,8 +113,6 @@ Effect& Effect::operator=(const Effect &obj) {
 
 istream& operator>>(istream& in, Effect& obj) {
     in>>(Image&)obj;
-    cout<<"Enter name for effect: \n";
-    in>>obj.name;
     cout<<"Are there effects applied on the image? (yes:1 no:0) \n";
     in>>obj.effect;
 
@@ -127,15 +121,76 @@ istream& operator>>(istream& in, Effect& obj) {
 
 ostream& operator<<(ostream& out, const Effect& obj) {
     out<<(Image&)obj;
-    out<<"Name of the effect: "<<obj.name<<endl;
     out<<"Has effects applied: "<<obj.effect<<endl;
 
     return out;
 }
 
 class Adjustment:public Image {
-
+private:
+    double brightness, contrast, hue;
+    bool adjustment;
+public:
+    Adjustment(string name = "Unnamed",string extension = "png",string path = "Root",double rezolutieX = 0,double rezolutieY = 0,
+               bool adjustment = false, double brightness = 0, double contrast = 0, double  hue = 0);
+    Adjustment(const Adjustment& obj);
+    Adjustment& operator=(const Adjustment& obj);
+    friend istream& operator>>(istream& in, Adjustment& obj);
+    friend ostream& operator<<(ostream& out, const Adjustment& obj);
 };
+
+Adjustment::Adjustment(string name, string extension, string path, double rezolutieX, double rezolutieY,
+                       bool adjustment, double brightness, double contrast, double hue):
+        Image(name,extension,path,rezolutieX,rezolutieY)
+{
+    this->adjustment = adjustment;
+    this->brightness = brightness;
+    this->contrast = contrast;
+    this->hue = hue;
+}
+
+Adjustment::Adjustment(const Adjustment &obj): Image(obj) {
+    this->adjustment = obj.adjustment;
+    this->brightness = obj.brightness;
+    this->contrast = obj.contrast;
+    this->hue = obj.hue;
+}
+
+Adjustment& Adjustment::operator=(const Adjustment &obj) {
+    if(this != &obj)
+    {
+        Image::operator=(obj);
+        this->adjustment = obj.adjustment;
+        this->brightness = obj.brightness;
+        this->contrast = obj.contrast;
+        this->hue = obj.hue;
+    }
+    return *this;
+}
+
+istream& operator>>(istream& in, Adjustment& obj) {
+    in>>(Image&)obj;
+    cout<<"Is the image adjusted? (yes:1 no:0)\n";
+    in>>obj.adjustment;
+    cout<<"Enter brightness [-100,100]: \n";
+    in>>obj.brightness;
+    cout<<"Enter contrast [-100,100]: \n";
+    in>>obj.contrast;
+    cout<<"Enter hue [0,255]: \n";
+    in>>obj.hue;
+
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Adjustment& obj) {
+    out<<(Image&)obj;
+    out<<"Has adjustments applied: "<<obj.adjustment<<endl;
+    out<<"Brightness value: "<<obj.brightness<<endl;
+    out<<"Contrast value: "<<obj.contrast<<endl;
+    out<<"Hue value: "<<obj.hue<<endl;
+
+    return out;
+}
 
 int main()
 {
@@ -172,6 +227,15 @@ int main()
     cout<<e<<endl;
     cin>>e;
     cout<<e<<endl;*/
+
+//    ADJUSTMENT TESTS
+    /*Adjustment a;
+    cout<<a<<endl;
+    cin>>a;
+    cout<<a<<endl;
+    Adjustment a2;
+    a2 = a;
+    cout<<a2<<endl;*/
 
 //  TODO method to find aspect ratio
     return 0;
