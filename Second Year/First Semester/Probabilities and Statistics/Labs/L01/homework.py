8e78b9fa0b09e4c7c14647706a38d5eb3361e9a7
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-07 14:09:48 +0300

    fix: added uniform function

--- Second Year/First Semester/Probabilities and Statistics/Labs/L01/homework.py
+++ Second Year/First Semester/Probabilities and Statistics/Labs/L01/homework.py
@@ -7,19 +7,38 @@ plt.ylim([-1,1])
 plt.axis('equal')
 plt.title('Puncte distribuite uniform intr-o elipsa')
 
-#semi-major axis length
-a = 1
-#semi-minor axis length
-b = 0.5
-#center of elipse
-center = (0,0)
+def method1():
+    #semi-major axis length
+    a = 1
+    #semi-minor axis length
+    b = 0.5
+    #center of elipse
+    center = (0,0)
 
-N = 1000
-for i in range(N):
-    x = np.random.uniform(-a,a)
-    y = np.random.uniform(-b,b)
+    N = 1000
+    for i in range(N):
+        x = np.random.uniform(-a,a)
+        y = np.random.uniform(-b,b)
 
-    if (x/a)**2 + (y/b)**2 <= 1:
+        if (x/a)**2 + (y/b)**2 <= 1:
+            plt.plot(x,y,'.')
+
+    plt.show()
+
+def method2():
+    a = 1
+    b = 0.5
+    N = 1000
+
+    for i in range(N):
+        theta = np.random.uniform(0,360)
+        r = np.sqrt(np.random.rand())
+
+        x = a/2 * r * np.cos(theta)
+        y = b/2 * r * np.sin(theta)
+
         plt.plot(x,y,'.')
 
-plt.show()
+    plt.show()
+
+method2()
