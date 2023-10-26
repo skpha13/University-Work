23b28ac1f7338a034652121ddc2f13be53b09fce
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-10-26 15:36:06 +0300

    feat: finished homework sql and exercise 2 from lab 04

---
+++ Second Year/First Semester/DBMS/Lab Exercises/Homework 04/main.sql
@@ -0,0 +1,63 @@
+-- exercitiul 9
+
+create or replace type filme as varray(5) of number(6);
+
+create table subscrpitii (
+    id number(6) primary key,
+    nume varchar2(20),
+    lista_filme filme
+)
+
+declare
+    v_filme filme := filme(1,2,3);
+    v_lista_filme subscrpitii.lista_filme%type;
+begin
+    insert into subscrpitii values (1,'basic',v_filme);
+    insert into subscrpitii values (2,'normal',filme(2,3,4));
+    insert into subscrpitii values (3,'premium',filme(1,5));
+
+    for i in 1..3 loop
+        select lista_filme
+        into v_lista_filme
+        from subscrpitii
+        where id = i;
+
+        DBMS_OUTPUT.PUT('Subscriptia ' || i || ': ');
+
+        for j in v_lista_filme.first..v_lista_filme.last loop
+            DBMS_OUTPUT.PUT(v_lista_filme(j) || ' ');
+            end loop;
+
+        DBMS_OUTPUT.NEW_LINE();
+        end loop;
+
+    delete subscrpitii;
+end;
+
+drop type filme;
+drop table subscrpitii;
+
+-- exercitiul 10
+
+create table utilizator_test as
+    select UTILIZATOR_ID, PORECLA
+    from UTILIZATOR
+    where ROWNUM <= 2;
+
+create or replace type tip_telefon is table of varchar(12);
+
+alter table utilizator_test
+add (telefon tip_telefon)
+nested table telefon store as tabel_telefon;
+
+insert into utilizator_test values (120,'El Dorado',tip_telefon('074XXX', '0213XXX', '037XXX'));
+
+update utilizator_test
+set telefon = tip_telefon('073XXX', '0214XXX')
+where UTILIZATOR_ID = 120;
+
+select u.UTILIZATOR_ID, t.*
+from utilizator_test u, table (u.telefon) t;
+
+drop table utilizator_test;
+drop type tip_telefon;
--- Second Year/First Semester/DBMS/Lab Exercises/L04/main.sql
+++ Second Year/First Semester/DBMS/Lab Exercises/L04/main.sql
@@ -140,11 +140,98 @@ END;
 -- metodele DELETE(n), DELETE(m,n) nu sunt valabile pentru vectori!!!
 -- din vectori nu se pot sterge elemente individuale!!!
 
-create or replace type tip_orase_ami is varray(10) of varchar2(20)
+create or replace type tip_orase_ami is varray(10) of varchar2(20);
+create or replace type excursiiMinime is varray(10) of number;
 
 create table excursie_ami (
-    cod_excursie number(4),
+    cod_excursie number(4) primary key ,
     denumire varchar2(20),
     orase tip_orase_ami,
-    status check ('disponibila','anulata')
+    status varchar2(20) default 'disponibila' check (lower(status) in ('disponibila','anulata'))
 );
+
+insert all
+    into excursie_ami values (1,'paradis',tip_orase_ami('napoli','capri','positano'),'disponibila')
+    into excursie_ami values (2,'vis',tip_orase_ami('paris','lyon'),'anulata')
+    into excursie_ami values (3,'munte',tip_orase_ami('sinaia','busteni','predeal','brasov'),'disponibila')
+    into excursie_ami values (4,'mare',tip_orase_ami('constinesti'),'disponibila')
+    into excursie_ami values (5,'excelent',tip_orase_ami('ploiesti','bucuresti'),'anulata')
+select 'worked' from dual;
+
+declare
+    v_excursie_id excursie_ami.cod_excursie%type := 1;
+    v_orase excursie_ami.orase%type;
+    v_nume_oras_1 varchar2(20) := 'napoli';
+    v_nume_oras_2 varchar2(20) := 'positano';
+    i number(4);
+    index1 number(4);
+    index2 number(4);
+    tempSwap varchar2(20);
+    v_excursiiMinime excursiiMinime;
+begin
+    select orase
+    into v_orase
+    from excursie_ami
+    where cod_excursie = v_excursie_id;
+
+    v_orase.extend();
+    v_orase(v_orase.last) := 'roma';
+
+    for i in v_orase.first..v_orase.last loop
+        DBMS_OUTPUT.PUT(v_orase(i) || ' ');
+        end loop;
+
+    DBMS_OUTPUT.NEW_LINE();
+
+    v_orase.extend();
+    i := v_orase.last;
+    while i > 2 loop
+        v_orase(i) := v_orase(i-1);
+        i := i-1;
+        end loop;
+    v_orase(2) := 'salermo';
+
+    for i in v_orase.first..v_orase.last loop
+        DBMS_OUTPUT.PUT(v_orase(i) || ' ');
+        end loop;
+
+    DBMS_OUTPUT.NEW_LINE();
+
+    for i in v_orase.first..v_orase.last loop
+        if v_orase(i) = v_nume_oras_1 then index1 := i;
+            end if;
+        if v_orase(i) = v_nume_oras_2 then index2 := i;
+            end if;
+        end loop;
+
+    tempSwap := v_orase(index1);
+    v_orase(index1) := v_orase(index2);
+    v_orase(index2) := tempSwap;
+
+    for i in v_orase.first..v_orase.last loop
+        DBMS_OUTPUT.PUT(v_orase(i) || ' ');
+        end loop;
+
+    DBMS_OUTPUT.NEW_LINE();
+
+    DBMS_OUTPUT.PUT('Excursie ' || v_excursie_id || ', Nr. Orase ' || v_orase.COUNT || ': ');
+    for i in v_orase.first..v_orase.last loop
+        DBMS_OUTPUT.PUT(v_orase(i) || ' ');
+        end loop;
+
+    DBMS_OUTPUT.NEW_LINE();
+
+    select denumire, orase
+    from excursie_ami;
+
+    -- nu stiu cum sa pun toate codurile in array
+    with ExcursieCounts as (select e.cod_excursie, count(*) as orase_count
+                            from excursie_ami e,
+                                 table ( e.orase ) t
+                            group by e.cod_excursie)
+    select cod_excursie
+    from ExcursieCounts
+    where orase_count = (select min(orase_count) from ExcursieCounts)
+    bulk collect into v_excursiiMinime;
+end;
+/
