a70f68192dc02917863ad27cf19de0373c494d17
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-11-13 18:31:25 +0200

    feat: new lab

---
+++ Second Year/First Semester/Functional Programming/Labs/L07/main.hs
@@ -0,0 +1,25 @@
+data Expr = Const Int
+        | Expr :+: Expr
+        | Expr :*: Expr
+        deriving Eq
+
+data Operation = Add | Mult deriving (Eq, Show)
+
+data Tree = Lf Int
+        | Node Operation Tree Tree
+        deriving (Eq, Show)
+
+evalExp :: Expr -> Int
+evalExp (Const x) = x
+evalExp (x :+: y) = evalExp x + evalExp y
+evalExp (x :*: y) = evalExp x * evalExp y
+
+exp1 = ((Const 2 :*: Const 3) :+: (Const 0 :*: Const 5))
+exp2 = (Const 2 :*: (Const 3 :+: Const 4))
+exp3 = (Const 4 :+: (Const 3 :*: Const 3))
+exp4 = (((Const 1 :*: Const 2) :*: (Const 3 :+: Const 1)) :*: Const 2)
+
+test11 = evalExp exp1 == 6
+test12 = evalExp exp2 == 14
+test13 = evalExp exp3 == 13
+test14 = evalExp exp4 == 16
