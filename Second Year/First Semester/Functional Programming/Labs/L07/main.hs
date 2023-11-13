3c2c78c79a05444b7a455c972a9adc70f4208ce0
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-11-13 19:14:02 +0200

    feat: finished expressions and trees

--- Second Year/First Semester/Functional Programming/Labs/L07/main.hs
+++ Second Year/First Semester/Functional Programming/Labs/L07/main.hs
@@ -23,3 +23,29 @@ test11 = evalExp exp1 == 6
 test12 = evalExp exp2 == 14
 test13 = evalExp exp3 == 13
 test14 = evalExp exp4 == 16
+
+evalArb :: Tree -> Int
+evalArb (Lf x) = x
+evalArb (Node op tree1 tree2) = case op of
+                                Add -> evalArb tree1 + evalArb tree2
+                                Mult -> evalArb tree1 * evalArb tree2
+
+arb1 = Node Add (Node Mult (Lf 2) (Lf 3)) (Node Mult (Lf 0)(Lf 5))
+arb2 = Node Mult (Lf 2) (Node Add (Lf 3)(Lf 4))
+arb3 = Node Add (Lf 4) (Node Mult (Lf 3)(Lf 3))
+arb4 = Node Mult (Node Mult (Node Mult (Lf 1) (Lf 2)) (Node Add (Lf 3)(Lf 1))) (Lf 2)
+
+test21 = evalArb arb1 == 6
+test22 = evalArb arb2 == 14
+test23 = evalArb arb3 == 13
+test24 = evalArb arb4 == 16
+
+expToArb :: Expr -> Tree
+expToArb (Const x) = Lf x
+expToArb (expr1 :+: expr2) = Node Add (expToArb expr1) (expToArb expr2) 
+expToArb (expr1 :*: expr2) = Node Mult (expToArb expr1) (expToArb expr2) 
+
+test1 = expToArb exp1 == arb1
+test2 = expToArb exp2 == arb2
+test3 = expToArb exp3 == arb3
+test4 = expToArb exp4 == arb4
