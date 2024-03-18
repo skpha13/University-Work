2dd5dc11c92972fc720a819eb6d46106d3969711
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-03-18 11:51:15 +0200

    feat: new lab

--- Second Year/Second Semester/Programming Language Fundamentals/Labs/L04/lab4.hs
+++ Second Year/Second Semester/Programming Language Fundamentals/Labs/L04/lab4.hs
@@ -193,7 +193,7 @@ andexp = do
             q <- npexpb
             return (AndE p q)
           
-data Stmt = AtrE String AExp | Seq Stmt Stmt | IfE BExp Stmt Stmt | WhileE BExp Stmt | Skip
+data Stmt = Skip | AtrE String AExp | Seq Stmt Stmt | IfE BExp Stmt Stmt | WhileE BExp Stmt
     deriving Show
 
 stmt :: Parser Stmt
@@ -294,137 +294,45 @@ bssos (Seq s1 s2) s = bssos s2 (bssos s1 s)
 bssos (IfE be s1 s2) s = if (valueb be s) then (bssos s1 s) else (bssos s2 s)
 bssos (WhileE be s1) s = if (valueb be s) then (bssos (WhileE be s1) (bssos s1 s)) else s
 
---This is where the new stuff starts
+prog = sum_no_p
 
-fix :: (a -> a) -> a
-fix f = f (fix f)
+test_bssos = bssos prog []
 
-factorialf :: Integer -> Integer
-factorialf = fix (\f n -> if n==0 then 1 else n * (f (n-1)))
+-- This is where the new stuff starts
 
-ackermannf :: Integer -> Integer -> Integer
-ackermannf = fix (\f m n -> if m == 0 then n + 1
-                            else  if n ==0 then f (m-1) 1
-                                    else f (m-1) (f m (n-1)))
+-- substitutes the QidX with the string by the the arithmetic expression
+substaexp :: AExp -> String -> AExp -> AExp
+substaexp = undefined
 
-factorial :: Integer -> Integer
-factorial n = if n==0 then 1 else n * (factorial (n-1))
+data Assn = BEX Bool | LEX AExp AExp | NotEX Assn | AndEX Assn Assn | DisjInfX [Assn]
 
-data Peano = Zero | Succ Peano deriving Show
+-- value of an assertion relative to a state, similar to valueb
+valueassn :: Assn -> [(String, Int)] -> Bool
+valueassn = undefined
 
-evenp Zero = True
-evenp (Succ n) = oddp n
-oddp Zero = False
-oddp (Succ n) = evenp n
+-- converts a boolean expression to an assertion
+convassn :: BExp -> Assn
+convassn = undefined
 
-evenoddf :: Peano -> (Bool, Bool)
-evenoddf = fix (\f nr -> case nr of
-                (Succ n) -> (snd(f n), fst(f n))
-                (Zero) -> (True, False)
-                )
+-- substitutes the QidX with the string by the the arithmetic expression
+substassn :: Assn -> String -> AExp -> Assn
+substassn = undefined
 
-testevenoddf = evenoddf (Succ (Succ (Succ Zero))) == (False, True)
+-- logical or
+orx :: Assn -> Assn -> Assn
+orx p q = NotEX (AndEX (NotEX p) (NotEX q))
 
-denot :: Stmt -> [(String, Int)] -> [(String, Int)]
-denot Skip = id
-denot (AtrE t e) = \ls -> update t (value e ls) ls  
-denot (Seq s1 s2) = \ls -> denot s2 (denot s1 ls)
-denot (IfE be s1 s2) = \ls -> case (valueb be ls) of 
-                                True -> denot s1 ls
-                                _ -> denot s2 ls
-denot (WhileE be s1) = fix (\f ls -> case (valueb be ls) of 
-                            True -> f (denot s1 ls)
-                            _ -> ls
-                        )             
+-- extracts the list
+extr :: Assn -> [Assn]
+extr (DisjInfX li) = li
 
+-- computes the weakest precondition
+wp :: Stmt -> Assn -> Assn
+wp = undefined
 
-prog = sum_no_p -- replace this with inf_cycle_p
+test1 = valueassn (wp prog (LEX (Qid "s") (Nu 5051))) [] -- should return true
 
-test_denot = (bssos prog []) == (denot prog [])
+test2 = valueassn (wp prog (LEX (Qid "s") (Nu 5050))) [] -- should return true
 
---Now general stuff starts
+test3 = valueassn (wp prog (LEX (Qid "s") (Nu 5049))) [] -- should not terminate
 
-type Algebra f a = f a -> a
-
-newtype Fix f = Fx (f (Fix f))
--- :k Fix
-
-unFix :: Fix f -> f (Fix f)
-unFix (Fx x) = x
-
-catamorphism :: Functor f => (f a -> a) -> (Fix f -> a)
-catamorphism a = fix (\f -> a . (fmap f). unFix)
--- this is the unique initial algebra morphism
-
--- Now specific stuff for propositional logic
-
-data ExprBoolF a = Var String | Neg a | Implies a a deriving Show
--- :k ExprBoolF
-
-instance Functor ExprBoolF where
-    fmap eval (Var v) = Var v
-    fmap eval (Neg phi) = Neg (eval phi)
-    fmap eval (Implies phi psi) = Implies (eval phi) (eval psi)
-
-type ExprBoolFix = Fix ExprBoolF
--- :k ExprBool
-
-instance Show ExprBoolFix where
-    show (Fx a) = "(Fx (" ++ show a ++ "))"
-
-sampleExpr :: ExprBoolFix
-sampleExpr = Fx (Implies (Fx (Var "a")) (Fx (Var "b")))
-
-type TargetBoolAlgebra = Algebra ExprBoolF ((String -> Bool) -> Bool)
-
-impl :: Bool -> Bool -> Bool
-impl a b = (not a) || b
-
-alg :: TargetBoolAlgebra
-alg (Var v) = ($ v)
-alg (Neg x) = not . x
-alg (Implies x y) = \e -> impl (x e) (y e)
-
-sampleEval :: String -> Bool
-sampleEval "a" = True
-sampleEval "b" = False
-
-test = catamorphism alg sampleExpr sampleEval == False
-
--- Now specific stuff for IMP
-          
-data StmtF a = AtrEF String AExp | SeqF a a | IfEF BExp a a | WhileEF BExp a | SkipF deriving Show
-
-type StmtFix = Fix StmtF
-
-instance Show StmtFix where
-    show (Fx a) = "(Fx (" ++ show a ++ "))"
-
-instance Functor StmtF where
-    fmap eval SkipF = SkipF
-    fmap eval (AtrEF t e) = AtrEF t e
-    fmap eval (SeqF s1 s2) = SeqF (eval s1) (eval s2)
-    fmap eval (IfEF be s1 s2) = IfEF be (eval s1) (eval s2)
-    fmap eval (WhileEF be s1) = WhileEF be (eval s1)
-
-type TargetStmtAlgebra = Algebra StmtF ([(String, Int)] -> [(String, Int)])
-
-algs :: TargetStmtAlgebra
-algs SkipF = undefined
-algs (AtrEF t e) = undefined
-algs (SeqF s1 s2) = undefined
-algs (IfEF be s1 s2) = undefined
-algs (WhileEF be s1) = fix (undefined)
-
--- Here we do a hack
-convstmt :: Stmt -> StmtFix
-convstmt Skip = Fx SkipF
-convstmt (AtrE t e) = Fx (AtrEF t e)
-convstmt (Seq s1 s2) = Fx (SeqF (convstmt s1) (convstmt s2))
-convstmt (IfE be s1 s2) = Fx (IfEF be (convstmt s1) (convstmt s2))
-convstmt (WhileE be s1) = Fx (WhileEF be (convstmt s1))
-
-progf :: StmtFix
-progf = convstmt prog
-
-testf = catamorphism algs progf []
