a149a24743ff7bad0fe0015f8de3be5dae7ede3f
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-05-20 20:16:54 +0300

    feat: done another problem

--- Second Year/Second Semester/Advanced Algorithms/Labs/L06/point_in_convex_polygon.cpp
+++ Second Year/Second Semester/Advanced Algorithms/Labs/L06/point_in_convex_polygon.cpp
@@ -52,32 +52,68 @@ string getAnswerString(Answer answer) {
     }
 }
 
-bool compare(pair<int,int> A, pair<int, int> B) {
-    return A.first < B.first || (A.first == B.first && A.second < B.second);
+class Point {
+public:
+    pair<int, int> point;
+    static pair<int, int> anchor;
+
+    Point() = default;
+    Point(pair<int, int> point) { this->point = point; }
+
+    Point operator+(const Point &p) const;
+    Point operator-(const Point &p) const;
+    long long crossProduct(const Point &p) const;
+    long long crossProduct(const Point &p1, const Point &p2) const ;
+    long long dotProduct(const Point &p) const;
+    long long dotProduct(const Point &p1, const Point &p2) const;
+    long long sqrtDistance() const { return this->dotProduct(*this); }
+};
+
+pair<int,int> Point::anchor;
+
+Point Point::operator+(const Point &p) const {
+    return {make_pair(this->point.first + p.point.first,
+                      this->point.second + p.point.second)};
 }
 
-long long crossProduct(pair<int, int> A, pair<int, int> B) {
-    return A.first * B.second - A.second * B.first;
+Point Point::operator-(const Point &p) const {
+    return {make_pair(this->point.first - p.point.first,
+                      this->point.second - p.point.second)};
 }
 
-long long crossProduct(pair<int, int> A, pair<int, int> B, pair<int, int> C) {
-    pair<int, int> newA, newB;
-
-    newA.first = B.first - A.first;
-    newA.second = B.second - A.second;
-
-    newB.first = C.first - A.first;
-    newB.second = C.second - A.second;
-
-    return crossProduct(newA, newB);
+long long Point::crossProduct(const Point &p) const {
+    return this->point.first * p.point.second - this->point.second * p.point.first;
 }
 
-Answer isInTriangle(pair<int, int> A, pair<int, int> B, pair<int, int> C, pair<int, int> point) {
-    long long s1 = abs(crossProduct(A, B, C));
-    long long s2 = abs(crossProduct(point, A, B)) + abs(crossProduct(point, B, C)) + abs(crossProduct(point, C, A));
+long long Point::crossProduct(const Point &p1, const Point &p2) const {
+    return (p1 - *this).crossProduct(p2 - *this);
+}
+
+long long Point::dotProduct(const Point &p) const {
+    return this->point.first * p.point.first + this->point.second * p.point.second;
+}
+
+bool compare(const Point &A, const Point &B) {
+    return A.point.first < B.point.first || (A.point.first == B.point.first && A.point.second < B.point.second);
+}
+
+int sign(long long value) {
+    if (value == 0)
+        return 0;
+
+    return value > 0 ? 1 : -1;
+}
+
+Answer isInTriangle(Point A, Point B, Point point) {
+    Point C = Point(make_pair(0, 0));
+    long long s1 = abs(A.crossProduct(B, C));
+    long long s2 = abs(point.crossProduct(A, B)) +
+                   abs(point.crossProduct(B, C)) +
+                   abs(point.crossProduct(C, A));
 
     if (s1 == s2) {
-        if (Orientation::orientationTest(A, B, point) == 0)
+        // check to see if point is situated on polygon
+        if (Orientation::orientationTest(A.point, B.point, point.point) == 0)
             return BOUNDARY;
 
         return INSIDE;
@@ -86,78 +122,84 @@ Answer isInTriangle(pair<int, int> A, pair<int, int> B, pair<int, int> C, pair<i
     return OUTSIDE;
 }
 
-Answer isInsideConvexPolygon(vector<pair<int, int>> &polygon, pair<int, int> point) {
-    int orientationWithPoint = Orientation::orientationTest(polygon[1], point, polygon[0]);
-    int orientationWithLastSegment = Orientation::orientationTest(polygon[1], polygon[polygon.size()-1], polygon[0]);
+Answer isInsideConvexPolygon(vector<Point> &polygon, Point point) {
+    // normalize point
+    point.point.first = point.point.first - Point::anchor.first;
+    point.point.second = point.point.second - Point::anchor.second;
 
-    if (orientationWithPoint != 0 && orientationWithPoint != orientationWithLastSegment)
+    // check for edge cases
+    if (polygon[0].crossProduct(point) != 0 &&
+        sign(polygon[0].crossProduct(point)) != sign(polygon[0].crossProduct(*(polygon.end()-1))))
         return OUTSIDE;
 
-    orientationWithPoint = Orientation::orientationTest(polygon[polygon.size()-1], point, polygon[0]);
-    int orientationWithFirstSegment = Orientation::orientationTest(polygon[polygon.size()-1], polygon[1], polygon[0]);
-
-    if (orientationWithPoint != 0 && orientationWithPoint != orientationWithFirstSegment)
+    if ((*(polygon.end()-1)).crossProduct(point) != 0 &&
+        sign((*(polygon.end()-1)).crossProduct(point)) != sign((*(polygon.end()-1)).crossProduct(polygon[0])))
         return OUTSIDE;
 
-    orientationWithPoint = Orientation::orientationTest(polygon[1], point, polygon[0]);
-    if (orientationWithPoint == 0 && (polygon[1].first * polygon[1].first + polygon[1].second * polygon[1].second) < (point.first * point.first + point.second * point.second)) {
+    if (polygon[0].crossProduct(point) == 0 && polygon[0].sqrtDistance() < point.sqrtDistance())
         return OUTSIDE;
-    }
 
-    int left = 1, right = polygon.size()-1, mid;
+    // binary search
+    int left = 0, right = polygon.size()-1, mid;
     while (left < right - 1) {
         mid = left + (right - left) / 2;
 
-        if (Orientation::orientationTest(polygon[mid], point, polygon[0]) <= 0)
+        // see orientation of generated line from anchor point
+        if (Orientation::orientationTest(polygon[mid].point, point.point, make_pair(0, 0)) <= 0)
             left = mid;
         else
             right = mid;
     }
 
     // check to see coliniarity on edge cases
-    if (left == 1 && isInTriangle(polygon[left], polygon[left+1], polygon[0], point) == INSIDE &&
-        (Orientation::orientationTest(polygon[0], polygon[left], point) == 0 ||
-         Orientation::orientationTest(polygon[left], polygon[left+1], point) == 0))
+    if (left == 0 && isInTriangle(polygon[left], polygon[left+1], point) != OUTSIDE &&
+        (Orientation::orientationTest(make_pair(0, 0), polygon[left].point, point.point) == 0 ||
+         Orientation::orientationTest(polygon[left].point, polygon[left+1].point, point.point) == 0))
     {
         return BOUNDARY;
     }
 
     // check to see coliniarity on edge cases
-    if (left == polygon.size()-2 && isInTriangle(polygon[left], polygon[left+1], polygon[0], point) == INSIDE &&
-        (Orientation::orientationTest(polygon[0], polygon[left+1], point) == 0 ||
-         Orientation::orientationTest(polygon[left], polygon[left+1], point) == 0))
+    if (left == polygon.size()-2 && isInTriangle(polygon[left], polygon[left+1], point) != OUTSIDE &&
+        (Orientation::orientationTest(make_pair(0, 0), polygon[left+1].point, point.point) == 0 ||
+         Orientation::orientationTest(polygon[left].point, polygon[left+1].point, point.point) == 0))
     {
         return BOUNDARY;
     }
 
-    return isInTriangle(polygon[left], polygon[left+1], polygon[0], point);
+    return isInTriangle(polygon[left], polygon[left+1], point);
 }
 
 int main() {
     int n, m;
-    vector<pair<int, int>> polygon;
+    vector<Point> polygon;
     cin >> n;
 
     int x, y;
     cin >> x >> y;
-    polygon.emplace_back(x, y);
+    polygon.emplace_back(make_pair(x, y));
     int smallestXIndex = 0;
 
     for (int i=1; i<n; i++) {
         cin >> x >> y;
-        polygon.emplace_back(x, y);
+        polygon.emplace_back(make_pair(x, y));
 
         if (compare(polygon[i], polygon[smallestXIndex]))
             smallestXIndex = i;
     }
     rotate(polygon.begin(), polygon.end() + smallestXIndex, polygon.end());
 
+    vector<Point> points(polygon.size()-1);
+    for (int i=0; i<points.size(); i++)
+        points[i] = polygon[i+1] - polygon[0];
+    Point::anchor = polygon[0].point;
+
     cin >> m;
     for (int i=0; i<m; i++) {
-        pair<int, int> R;
-        cin >> R.first >> R.second;
+        Point R;
+        cin >> R.point.first >> R.point.second;
 
-        cout << getAnswerString(isInsideConvexPolygon(polygon, R)) << endl;
+        cout << getAnswerString(isInsideConvexPolygon(points, R)) << endl;
     }
 
     return 0;
