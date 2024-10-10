d4339ea0da604381096c22c1130fbed05900bdd7
Adrian Mincu <adrian-lucian.mincu@s.unibuc.ro>
2024-10-10 22:27:19 +0300

    refactore code

---
+++ Third Year/First Semester/Numerical Computing in Computer Science/Labs/L01/proj/exercise_01.py
@@ -0,0 +1,20 @@
+"""Exercise 01"""
+
+from series import generate_series
+
+print("\tExercise 01:")
+x0 = 0.1
+k = 60
+series = generate_series(x0, k)
+print(f"(a), (b)\n{k} = {series[-1]}\n")
+
+print(f"(c)")
+for number in series:
+    print(f"{number:.4f}", end=" ")
+print("\n")
+"""
+(c)
+
+Diferentele apareau din cauza cumularii erorii de precizie
+Dar dupa ce am adaugat rounding la fiecare numar am reusit sa scapam de aceasta problema
+"""
---
+++ Third Year/First Semester/Numerical Computing in Computer Science/Labs/L01/proj/exercise_02.py
@@ -0,0 +1,38 @@
+"""Exercise 02"""
+
+import numpy as np
+
+from matrix import UTRIS, gaussian_elimination
+from series import generate_series
+
+print("\tExercise 02:")
+print("(a)")
+n = 3
+A = np.array([[2, 4, -2], [4, 9, -3], [-2, -3, 7]])
+b = np.array([2, 8, 10])
+print("A:")
+print(A)
+
+A_gauss = gaussian_elimination(A, b)
+print("\nA Gauss:")
+print(A_gauss)
+print("\nb Gauss:")
+print(b)
+
+print("\n(b)")
+print("X:")
+print(UTRIS(A_gauss, b))
+
+
+test_matrix = np.random.randint(2, 10, (6, 6))
+test_b = np.random.randint(2, 10, 6)
+
+print("\n(c)")
+print("6x6:")
+print(test_matrix)
+print("\n6x6 Gauss")
+test_matrix_gauss = gaussian_elimination(test_matrix, test_b)
+print(test_matrix_gauss)
+
+print("\nX:")
+print(UTRIS(test_matrix_gauss, test_b))
