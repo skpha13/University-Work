5fad6f97693f04e0f6d32ced6f83c63f36d6572f
Adrian Mincu <adrian-lucian.mincu@s.unibuc.ro>
2024-10-24 19:22:16 +0300

    refactored code

--- Third Year/First Semester/Signal Processing/Labs/L04/proj/exercise_02.py
+++ Third Year/First Semester/Signal Processing/Labs/L04/proj/exercise_02.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,6 +9,8 @@ dir_path = f"{os.getcwd()}/plots"
 if not os.path.exists(dir_path):
     os.makedirs(dir_path)
 
+plt.rc("axes", titlesize=10)
+
 t = 1
 n = 1000
 ns = 7
@@ -15,28 +18,32 @@ ns = 7
 x_domain = np.linspace(0, t, n)
 nts = np.linspace(0, t, ns)
 
-base_signal_16 = sinusoidal_signal(1, 16, t, 0, 1000)
-samples_signal_16 = sinusoidal_signal(1, 16, t, 0, ns)
 
-base_signal_8 = sinusoidal_signal(1, 10, t, 0, 1000)
-samples_signal_8 = sinusoidal_signal(1, 10, t, 0, ns)
+@dataclass
+class Signals:
+    frequency: str
+    plot: np.ndarray
+    scatter: np.ndarray
+    color: str
 
-base_signal_4 = sinusoidal_signal(1, 4, t, 0, 1000)
-samples_signal_4 = sinusoidal_signal(1, 4, t, 0, ns)
+
+signals: list[Signals] = [
+    Signals("16Hz", sinusoidal_signal(1, 16, t, 0, 1000), sinusoidal_signal(1, 16, t, 0, ns), "slateblue"),
+    Signals("10Hz", sinusoidal_signal(1, 10, t, 0, 1000), sinusoidal_signal(1, 10, t, 0, ns), "purple"),
+    Signals("4Hz", sinusoidal_signal(1, 4, t, 0, 1000), sinusoidal_signal(1, 4, t, 0, ns), "green"),
+]
 
 fig, axs = plt.subplots(4)
 fig.suptitle("Sub-Nyquist Frequencies")
 
-axs[0].plot(x_domain, base_signal_16, color="slateblue")
+axs[0].plot(x_domain, signals[0].plot, color="slateblue")
+axs[0].set_title(signals[0].frequency)
 
-axs[1].plot(x_domain, base_signal_16, color="slateblue", zorder=1)
-axs[1].scatter(nts, samples_signal_16, color="yellow", alpha=1, zorder=2)
+for ax, signal in zip(axs[1:], signals):
+    ax.plot(x_domain, signal.plot, color=signal.color, zorder=1)
+    ax.scatter(nts, signal.scatter, color="yellow", alpha=1, zorder=2)
+    ax.set_title(signal.frequency)
 
-axs[2].plot(x_domain, base_signal_8, color="purple", zorder=1)
-axs[2].scatter(nts, samples_signal_8, color="yellow", alpha=1, zorder=2)
-
-axs[3].plot(x_domain, base_signal_4, color="green", zorder=1)
-axs[3].scatter(nts, samples_signal_4, color="yellow", alpha=1, zorder=2)
 
 plt.tight_layout()
 plt.savefig("plots/Sub-Nyquist Frequencies.pdf", format="pdf")
