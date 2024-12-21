7403b4c51f67b7171ee1de5dc7e22ce7c1437380
skpha13 <mal13adi03@gmail.com>
2024-12-21 16:12:32 +0200

    fixed issue with U and V multiplication

--- Third Year/First Semester/Signal Processing/Labs/L11/proj/exercise_04.py
+++ Third Year/First Semester/Signal Processing/Labs/L11/proj/exercise_04.py
@@ -1,6 +1,11 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import root_mean_squared_error
 from utils.signal import dehankelize_matrix, hankelize_matrix, hankelize_series, predefined_series
+
+os.makedirs(f"{os.getcwd()}/plots", exist_ok=True)
 
 
 def main():
@@ -14,17 +19,30 @@ def main():
 
     hankelized_matrix = np.zeros(V.shape)
     for i in range(len(S)):
-        Xi = S[i] * np.outer(U[i].T, V[i])
+        Xi = S[i] * np.outer(U.T[i], V[i])
 
         hankelized_xi = hankelize_matrix(Xi)
         hankelized_matrix += hankelized_xi
 
     dehankelized_matrix = dehankelize_matrix(hankelized_matrix)
 
-    print(dehankelized_matrix.shape)
+    error = root_mean_squared_error(series, dehankelized_matrix)
 
-    plt.plot(np.linspace(0, 1, dehankelized_matrix.shape[0]), series)
-    plt.plot(np.linspace(0, 1, dehankelized_matrix.shape[0]), dehankelized_matrix)
+    print(f"Difference between Series and SSA: {error}")
+
+    plot_name = "Singular Spectrum Analysis"
+    plt.title(plot_name)
+
+    preview_offset = 600
+    preview_index = 700
+    xs = np.linspace(0, 1, preview_index)
+
+    plt.plot(xs[preview_offset:], series[preview_offset:preview_index])
+    plt.plot(xs[preview_offset:], dehankelized_matrix[preview_offset:preview_index], color="red")
+
+    plt.legend(["Raw Series", "SSA"])
+
+    plt.savefig(f"plots/{plot_name}.svg", format="svg")
     plt.show()
 
 
